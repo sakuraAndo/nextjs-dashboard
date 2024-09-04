4646220a971d51d74dbf4f397d5fb10736aec023
sakuraAndo <494912633@qq.com>
2024-09-04 17:37:16 +0800

    [main] : add customer page
    【提交描述】：补充用户页面

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,17 @@
-export default function Page() {
-    return <p>Customers Page</p>
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export default async function Page({
+    searchParams,
+  }: {
+    searchParams?: {
+      query?: string;
+      page?: string;
+    };
+  }) {
+    const query = searchParams?.query || '';
+  
+    const customers = await fetchFilteredCustomers(query);
+    
+    return <Table customers={customers}/>
 }
